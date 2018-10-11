95680db79196f41e24970d67ec86f871ec27731f
dannll <36960555+dannll@users.noreply.github.com>
2018-10-11 18:29:16 +0300

    Добавил валидацию формы в modal-login и сделал focus на логин

--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,26 @@
 var link = document.querySelector(".login-link-ref");
 var popup = document.querySelector(".modal-login");
 var closeBtn = document.querySelector(".modal-close");
+var modalLogin = document.querySelector(".user-form input[name=\"login\"]");
+var modalForm = document.querySelector(".user-form");
+var modalPassword = document.querySelector(".user-form input[type=\"password\"]");
 
 link.addEventListener("click", function(evt) {
   evt.preventDefault();
-
   popup.classList.add("modal-show");
+  modalLogin.focus();
 });
 
 closeBtn.addEventListener("click", function(evt) {
   evt.preventDefault();
 
   popup.classList.remove("modal-show");
-})
+});
+
+modalForm.addEventListener("submit", function(evt) {
+
+  if (!modalLogin.value || !modalPassword.value) {
+    evt.preventDefault();
+    console.log("Отсутствует значение логина и пароля");
+  }
+});
