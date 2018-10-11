5939e655f1f8cb1affe392eea32e013cb7f8e8d8
dannll <36960555+dannll@users.noreply.github.com>
2018-10-11 21:33:23 +0300

    Добавил поддержку localStorage

--- js/popup.js
+++ js/popup.js
@@ -4,11 +4,24 @@ var closeBtn = document.querySelector(".modal-close");
 var modalLogin = document.querySelector(".user-form input[name=\"login\"]");
 var modalForm = document.querySelector(".user-form");
 var modalPassword = document.querySelector(".user-form input[type=\"password\"]");
+var isStorageSupport = true;
+var storageLogin = "";
+
+try {
+  storageLogin = localStorage.getItem("login");
+} catch {
+  isStorageSupport = false;
+}
 
 link.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
-  modalLogin.focus();
+  if (storageLogin) {
+    modalLogin.value = storageLogin;
+    modalPassword.focus();
+  } else {
+    modalLogin.focus();
+  }
 });
 
 closeBtn.addEventListener("click", function(evt) {
@@ -22,5 +35,9 @@ modalForm.addEventListener("submit", function(evt) {
   if (!modalLogin.value || !modalPassword.value) {
     evt.preventDefault();
     console.log("Отсутствует значение логина и пароля");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", modalLogin.value);
+    }
   }
 });
